85f720dab0f889e7b48ae8b7cf10622428fac61f
Sebastian Kopec <github.com@soften.pl>
2023-02-11 12:55:55 +0100

    Once pressed keys in user input

--- src/modules/userInput.ts
+++ src/modules/userInput.ts
@@ -5,15 +5,21 @@ export default class UserInput {
     mouseX: number
     mouseY: number
     mouseClick: boolean
+    keyPressed: string[]
+    keyPressedOnce: string[]
 
     constructor(game: Game) {
         this.game = game
         this.mouseX = 0
         this.mouseY = 0
         this.mouseClick = false
+        this.keyPressed = []
+        this.keyPressedOnce = []
         window.addEventListener('mousemove', this.getMousePosition.bind(this), false)
         window.addEventListener('mousedown', this.getMouseDown.bind(this), false)
         window.addEventListener('mouseup', this.getMouseUp.bind(this), false)
+        window.addEventListener('keydown', this.getKeyDown.bind(this), false)
+        window.addEventListener('keyup', this.getKeyUp.bind(this), false)
     }
 
     getMousePosition(event: MouseEvent)   {        
@@ -28,6 +34,36 @@ export default class UserInput {
 
     getMouseUp() {
         this.mouseClick = false
+    }
+
+    getKeyDown(event: KeyboardEvent) {
+        
+        if(!this.keyPressed.includes(event.code)) {
+            this.keyPressed.push(event.code)
+            
+            if(!this.keyPressedOnce.includes(event.code)) {
+                this.keyPressedOnce.push(event.code)
+            }
+        }
+    }
+
+    getKeyUp(event: KeyboardEvent) {
+        if(this.keyPressed.includes(event.code)) {
+            const idx = this.keyPressed.findIndex(item => item === event.code)
+            this.keyPressed.splice(idx, 1)
+        }
+    }
+
+    resetInput() {
+        return this.keyPressedOnce = []
+    }
+
+    isKeyPressed(key: string) {
+        return this.keyPressed.includes(key)
+    }
+
+    isKeyPressedOnce(key: string) {
+        return this.keyPressedOnce.includes(key)
     }
 
 }
