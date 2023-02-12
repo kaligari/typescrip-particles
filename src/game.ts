73d70ed04fa7e0d8a3da22ff1040dd987d12c0d6
Sebastian Kopec <github.com@soften.pl>
2023-02-12 16:26:34 +0100

    feat: Simpler init

--- src/game.ts
+++ src/game.ts
@@ -1,20 +1,12 @@
 import rendererEngine from '@/rendererEngine'
 export default class Game {
     constructor(width: number, height: number, scale = 1) {
-        window.addEventListener('resize', () => {
-            this.init(width, height, scale)
-        })
-
-        this.init(width, height, scale)
+        rendererEngine.init(width, height, scale)
+        this.render()
     }
 
     render() {
         rendererEngine.render()
         window.requestAnimationFrame(this.render.bind(this))
-    }
-
-    init(width: number, height: number, scale: number) {
-        rendererEngine.init(width, height, scale)
-        this.render()
     }
 }
