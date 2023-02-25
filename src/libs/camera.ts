ce60443f3e3f4d5ceb7c0151f1b81125f6ed7413
Sebastian Kopec <github.com@soften.pl>
2023-02-25 18:54:42 +0100

    feat: Extend camera class

--- src/libs/camera.ts
+++ src/libs/camera.ts
@@ -1,6 +1,22 @@
 class Camera {
-    x = 0
-    y = 0
+    private _x = 0
+    private _y = 0
+
+    get x() {
+        return this._x
+    }
+
+    set x(value: number) {
+        this._x = value
+    }
+
+    get y() {
+        return this._y
+    }
+
+    set y(value: number) {
+        this._y = value
+    }
 }
 
 const camera = new Camera()
